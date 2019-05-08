606db748a6db46a86bd46edb754d0caa6cfc048c
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-05-08 11:37:48 -0500

    completed reset

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 
 class RockCounter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rocksPicked: 0
+    }
+  }
+
+  clickForMoreRocks = () => {
+    this.setState({
+      rocksPicked: this.state.rocksPicked + 1,
+    })
+  }
+
+  clickForLessRocks = () => {
+    this.setState({
+      rocksPicked: this.state.rocksPicked - 1,
+    })
+  }
+
+  resetRocks = () => {
+    this.setState({
+      rocksPicked: 0,
+    })
+  }
+
   render() {
     return (
       <div>
-          <div>
-              Rocks Picked: 0
-          </div>
-          <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
-          </div>
+        <div>
+          Rocks Picked: {this.state.rocksPicked}
+        </div>
+        <div>
+          <button onClick={this.clickForMoreRocks}>Increase</button>
+          <button onClick={this.clickForLessRocks}>Decrease</button>
+          <button onClick={this.resetRocks}>Reset</button>
+        </div>
       </div>
     );
   }
